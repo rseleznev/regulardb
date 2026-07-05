85d4b231012ee0f0397f09289856cd84dc152115
rseleznev <gradoozd@gmail.com>
2026-07-05 12:20:55 +0300

    Получилось записать и прочитать из файла

--- main.c
+++ main.c
@@ -36,7 +36,16 @@ int main(void) {
     free(table_info);
 
     // чтение каталога
-    TableInfo* buf;
+    TableInfo* buf = malloc(sizeof(TableInfo) + sizeof(TableColumn)*1);
+    if (buf == NULL) {
+        printf("ошибка выделения памяти для buf \n");
+        return 1;
+    }
+    buf->name = malloc(100);
+    buf->file_name = malloc(100);
+    buf->columns[0].name = malloc(100);
+    buf->columns[0].type = malloc(100);
+
     res = get_catalog(buf);
     if (res != 0) {
         return 1;
@@ -48,6 +57,12 @@ int main(void) {
     printf("\t name: %s \n", buf->columns[0].name);
     printf("\t type: %s \n", buf->columns[0].type);
     printf("\t len: %d \n", buf->columns[0].len);
+
+    free(buf->columns[0].name);
+    free(buf->columns[0].type);
+    free(buf->name);
+    free(buf->file_name);
+    free(buf);
 
     return 0;
 }
