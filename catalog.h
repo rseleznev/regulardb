e76409f25e872b2f46a491de095a3842b86a84eb
rseleznev <gradoozd@gmail.com>
2026-07-05 14:09:51 +0300

    Сделал структуру Каталог и задал фикс размеры строкам других структур

--- catalog.h
+++ catalog.h
@@ -2,20 +2,25 @@
 #define REGULARDB_CATALOG_H
 
 typedef struct TableColumn {
-    char* name;
-    char* type;
-    unsigned int len;
+    char name[25];
+    char type[25];
+    unsigned int value_len;
 } TableColumn;
 
 typedef struct TableInfo {
-    char* name;
-    char* file_name;
-    int columns_count;
+    char name[25];
+    char file_name[25];
+    unsigned int columns_len;
     TableColumn columns[];
 } TableInfo;
 
+typedef struct Catalog {
+    unsigned int tables_len;
+    TableInfo tables[];
+} Catalog;
+
 int create_catalog(void);
 int append_catalog(TableInfo* table_info);
-int get_catalog(TableInfo* buf);
+int get_catalog(Catalog* catalog);
 
 #endif
