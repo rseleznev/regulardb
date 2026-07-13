536e224a6b52ae638f054197ca6cf94d78c672cc
rseleznev <gradoozd@gmail.com>
2026-07-13 15:22:10 +0300

    Сделал функцию для создания файла с пустой страницей

--- main.c
+++ main.c
@@ -2,8 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 
+int create_file(char* file_name);
+
 int main(void) {
-    Page* page = get_page("db/tables/users.rdb", 1);
+    int res = create_file("db/tables/users.rdb");
+    if (res != 0) {
+        return 1;
+    }
+    
+    Page* page = get_page("db/tables/users.rdb", 2);
     if (page == NULL) {
         page = new_page("db/tables/users.rdb");
         if (page == NULL) {
@@ -18,9 +25,26 @@ int main(void) {
     }
     printf("\n");
 
-    strcpy(page->data, "test value");
+    strcpy(page->data, "page 1 value");
     page->changed = true;
     save_page(page);
+
+    /* Разобраться, почему удаляется страница 2, когда записываем страницу 1 */
+
+    return 0;
+}
+
+int create_file(char* file_name) {
+    FILE* f = fopen("db/tables/users.rdb", "wb");
+    if (!f) {
+        return -1;
+    }
+
+    char zeros[PAGE_LEN] = {0};
+    size_t n = fwrite(zeros, 1, PAGE_LEN, f);
+    if (n != PAGE_LEN) {
+        return -1;
+    }
 
     return 0;
 }
