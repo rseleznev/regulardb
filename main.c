c91718f64cefbdde51092340b3618df39500d050
rseleznev <gradoozd@gmail.com>
2026-07-22 16:42:07 +0300

    Сделал добавление записей на страницу

--- main.c
+++ main.c
@@ -5,12 +5,12 @@
 #include <sys/types.h>
 
 typedef struct PageHeader {
-    off_t upper_idx;
-    off_t lower_idx;
+    off_t upper_idx; // позиция свободного байта сверху для записи RecordInfo
+    off_t lower_idx; // позиция свободного байта снизу для записи данных
 } PageHeader;
 
 typedef struct RecordInfo {
-    off_t end_offset;
+    off_t data_idx;
     size_t data_len;
     char status;
 } RecordInfo;
@@ -18,6 +18,10 @@ typedef struct RecordInfo {
 int create_file(char* file_name);
 void init_page_header(Page* page);
 PageHeader* read_page_header(Page* page);
+void update_page_header(Page* page, PageHeader* header);
+
+int append_record(char* data, int len, Page* page);
+
 void print_8_bits(char c);
 void print_64_bits(long n);
 
@@ -42,8 +46,14 @@ int main(void) {
     if (hdr == NULL) {
         return 1;
     }
-    printf("header.upper: %ld \n", hdr->upper_idx);
-    printf("header.lower: %ld \n", hdr->lower_idx);
+    printf("header.upper_idx: %ld \n", hdr->upper_idx);
+    printf("header.lower_idx: %ld \n", hdr->lower_idx);
+
+    // char* data = "random test value";
+    // res = append_record(data, 17, page);
+    // if (res != 0) {
+    //     return 1;
+    // }
 
     // init_page_header(page);
     // page->changed = true;
@@ -76,13 +86,13 @@ void init_page_header(Page* page) {
 
     shift = 0;
     for (i = 0; i < sizeof(header.upper_idx); i++) {
-        page->data[i] = (char)(header.upper_idx >> shift);
+        page->data[i] = (unsigned char)(header.upper_idx >> shift);
         shift += 8;
     }
 
     shift = 0;
     for (; i < sizeof(PageHeader); i++) {
-        page->data[i] = (char)(header.lower_idx >> shift);
+        page->data[i] = (unsigned char)(header.lower_idx >> shift);
         shift += 8;
     }
 }
@@ -108,6 +118,80 @@ PageHeader* read_page_header(Page* page) {
     }
 
     return header;
+}
+
+void update_page_header(Page* page, PageHeader* header) {
+    int i, shift;
+
+    shift = 0;
+    for (i = 0; i < sizeof(header->upper_idx); i++) {
+        page->data[i] = (unsigned char)(header->upper_idx >> shift);
+        shift += 8;
+    }
+
+    shift = 0;
+    for (; i < sizeof(PageHeader); i++) {
+        page->data[i] = (unsigned char)(header->lower_idx >> shift);
+        shift += 8;
+    }
+}
+
+int append_record(char* data, int len, Page* page) {
+    if (data == NULL) {
+        printf("append_record: data is NULL \n");
+        return -1;
+    }
+    if (page == NULL) {
+        printf("append_record: page is NULL \n");
+        return -1;
+    }
+    
+    PageHeader* hdr = read_page_header(page);
+    if (hdr == NULL) {
+        printf("append_record: header is NULL \n");
+        return -1;
+    }
+
+    if (hdr->upper_idx + len > hdr->lower_idx - len) {
+        printf("append_record: not enough space for record \n");
+        return -1;
+    }
+
+    // записываем RecordInfo
+    RecordInfo record_info;
+    record_info.data_idx = hdr->lower_idx - len;
+    record_info.data_len = len;
+    record_info.status = 'a';
+
+    int i, shift;
+
+    shift = 0;
+    for (i = 0; i < sizeof(record_info.data_idx); i++) {
+        page->data[hdr->upper_idx] = (unsigned char)(record_info.data_idx >> shift);
+        shift += 8;
+        hdr->upper_idx++;
+    }
+
+    shift = 0;
+    for (i = 0; i < sizeof(record_info.data_len); i++) {
+        page->data[hdr->upper_idx] = (unsigned char)(record_info.data_len >> shift);
+        shift += 8;
+        hdr->upper_idx++;
+    }
+
+    page->data[hdr->upper_idx] = record_info.status;
+    hdr->upper_idx++;
+
+    // записываем данные
+    for (i = 0; i < len; i++) {
+        page->data[hdr->lower_idx - len + i] = data[i];
+    }
+
+    // обновление индексов заголовка
+    hdr->lower_idx = hdr->lower_idx - len - 1;
+    update_page_header(page, hdr);
+
+    return 0;
 }
 
 void print_8_bits(char c) {
